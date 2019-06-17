e7aeb435969c3241b736f16c12a2ab02c0b0c928
Mando <maggicb910@gmail.com>
2019-06-17 18:00:58 +0800

    190617_上課內容

---
+++ mando03.js
@@ -0,0 +1,18 @@
+let var1;
+document.write(var1 + "<br>");
+var1 =123;//初始化
+document.write(typeof(var1));
+var1 =1.23;//指派
+document.write(typeof(var1));
+//變數,型態．JS屬於弱型別語言（java 相反）
+// let var1; //宣告變數
+// let var2 = 123; //宣告變數同時給值
+//[a-zA-Z$_]->出現一次 [a-zA-Z0-9$_]*->可出現多次
+// document.write($_$);
+// ”等號“任何的變數、常數第一次給值為初始化
+// 常數不可以進行指派Assignment
+const name = "MANDO";
+document.write("my name is" + name + "<br>");
+document.write(`My name is ${MANDO}`);
+let a = 10, b=3;
+document.write(`${a}+${b}=${a+b}`);
